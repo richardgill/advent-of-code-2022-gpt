f4f54e7468383aad79a9fd6ef55044150e4bc7a6
Richard Gill <richard@rgill.co.uk>
2022-12-02 08:14:29 +0000

    day 2

--- src/days/02.1/index.js
+++ src/days/02.1/index.js
@@ -3,11 +3,48 @@ import { readRelativeInput } from '@/common/file.js';
 
 const readInput = (fileName) => readRelativeInput(import.meta.url, fileName);
 
+const numberToActualMove = {
+  [1]: 'Rock',
+  [2]: 'Paper',
+  [3]: 'Scissors',
+};
+
+const scoreMyMove = (myMove) => {
+  return myMove.charCodeAt(0) - (64 + 23);
+};
+
+const scoreTheirMove = (myMove) => {
+  return myMove.charCodeAt(0) - 64;
+};
+
+const scoreResult = (theirMove, myMove) => {
+  if (scoreMyMove(myMove) - scoreTheirMove(theirMove) === 0) {
+    return 3;
+  }
+  if ([1, -2].includes(scoreMyMove(myMove) - scoreTheirMove(theirMove))) {
+    return 6;
+  } else if ([-1, 2].includes(scoreMyMove(myMove) - scoreTheirMove(theirMove))) {
+    return 0;
+  }
+  throw new Error(`never happens myMove: ${myMove} theirMove: ${theirMove} res: ${scoreMyMove(myMove) - scoreTheirMove(theirMove)}`);
+};
+
+const scoreMove = ([theirMove, myMove]) => {
+  console.log(
+    theirMove,
+    myMove,
+    numberToActualMove[scoreTheirMove(theirMove)],
+    numberToActualMove[scoreMyMove(myMove)],
+    scoreResult(theirMove, myMove),
+  );
+  return scoreMyMove(myMove) + scoreResult(theirMove, myMove);
+};
+
 export const solve = (input) => {
-  console.log(input);
-  return 'answer';
+  const moves = input.trim().split('\n').map((line) => line.split(' '));
+  return _.chain(moves).map(scoreMove).sum().value();
 };
 
 console.log(solve(readInput('example1.txt')), '\n\n\n');
 // console.log(solve(readInput('example2.txt')), '\n\n\n');
-// console.log(solve(readInput('puzzleInput.txt')), '\n\n\n');
+console.log(solve(readInput('puzzleInput.txt')), '\n\n\n');
--- src/days/02.2/index.js
+++ src/days/02.2/index.js
@@ -3,11 +3,52 @@ import { readRelativeInput } from '@/common/file.js';
 
 const readInput = (fileName) => readRelativeInput(import.meta.url, fileName);
 
+const scoreTheirMove = (myMove) => {
+  return myMove.charCodeAt(0) - 64;
+};
+
+const scoreResult = (theirMove, myMove) => {
+  if (myMove - theirMove === 0) {
+    return 3;
+  }
+  if ([1, -2].includes(myMove - theirMove)) {
+    return 6;
+  } else if ([-1, 2].includes(myMove - theirMove)) {
+    return 0;
+  }
+  throw new Error(`never happens myMove: ${myMove} theirMove: ${theirMove} res: ${myMove - theirMove}`);
+};
+
+const calculateRequiredScore = (requiredOutcome) => {
+  if (requiredOutcome === 'X') {
+    return 0;
+  } else if (requiredOutcome === 'Y') {
+    return 3;
+  } else {
+    return 6;
+  }
+};
+
+const calculateMyMoveScore = (theirMoveScore, requiredOutcome) => {
+  const requiredScore = calculateRequiredScore(requiredOutcome);
+  for (const myScore of [1, 2, 3]) {
+    if (scoreResult(theirMoveScore, myScore) === requiredScore) {
+      return myScore;
+    }
+  }
+};
+
+const scoreMove = ([theirMove, myMove]) => {
+  const theirMoveScore = scoreTheirMove(theirMove);
+  const myMoveScore = calculateMyMoveScore(theirMoveScore, myMove);
+
+  return myMoveScore + scoreResult(theirMoveScore, myMoveScore);
+};
+
 export const solve = (input) => {
-  console.log(input);
-  return 'answer';
+  const moves = input.trim().split('\n').map((line) => line.split(' '));
+  return _.chain(moves).map(scoreMove).sum().value();
 };
 
 console.log(solve(readInput('example1.txt')), '\n\n\n');
-// console.log(solve(readInput('example2.txt')), '\n\n\n');
-// console.log(solve(readInput('puzzleInput.txt')), '\n\n\n');
+console.log(solve(readInput('puzzleInput.txt')), '\n\n\n');
