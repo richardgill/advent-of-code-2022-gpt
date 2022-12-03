bec0c59e78c86f7f31dfff49facbd81e979914dd
Richard Gill <richard@rgill.co.uk>
2022-12-03 09:09:01 +0000

    day 3

--- src/days/03.1/index.js
+++ src/days/03.1/index.js
@@ -3,11 +3,30 @@ import { readRelativeInput } from '@/common/file.js';
 
 const readInput = (fileName) => readRelativeInput(import.meta.url, fileName);
 
+const letterToPriority = (letter) => {
+  if (/[A-Z]/.test(letter)) {
+    return letter.charCodeAt(0) - 38;
+  }
+  return letter.charCodeAt(0) - 96;
+};
+
+const findDuplicate = (rucksack) => {
+  const compartmentA = rucksack.slice(0, rucksack.length / 2);
+  const compartmentB = rucksack.slice(rucksack.length / 2, rucksack.length);
+  const duplicate = _.intersection(compartmentA, compartmentB);
+  console.log(compartmentA, compartmentB);
+  console.log(duplicate);
+  if (duplicate.length !== 1) {
+    throw new Error('duplicate.length !== 1');
+  }
+  return duplicate[0];
+};
+
 export const solve = (input) => {
-  console.log(input);
-  return 'answer';
+  const rucksacks = input.trim().split('\n').map((line) => line.split('').map((x) => x));
+  return _.chain(rucksacks).map(findDuplicate).map(letterToPriority).sum().value();
 };
 
 console.log(solve(readInput('example1.txt')), '\n\n\n');
 // console.log(solve(readInput('example2.txt')), '\n\n\n');
-// console.log(solve(readInput('puzzleInput.txt')), '\n\n\n');
+console.log(solve(readInput('puzzleInput.txt')), '\n\n\n');
--- src/days/03.2/index.js
+++ src/days/03.2/index.js
@@ -3,11 +3,26 @@ import { readRelativeInput } from '@/common/file.js';
 
 const readInput = (fileName) => readRelativeInput(import.meta.url, fileName);
 
+const letterToPriority = (letter) => {
+  if (/[A-Z]/.test(letter)) {
+    return letter.charCodeAt(0) - 38;
+  }
+  return letter.charCodeAt(0) - 96;
+};
+
+const findDuplicate = (rucksacks) => {
+  const duplicate = _.intersection(...rucksacks);
+  if (duplicate.length !== 1) {
+    throw new Error('duplicate.length !== 1');
+  }
+  return duplicate[0];
+};
+
 export const solve = (input) => {
-  console.log(input);
-  return 'answer';
+  const rucksacks = input.trim().split('\n').map((line) => line.split('').map((x) => x));
+  return _.chain(rucksacks).chunk(3).map(findDuplicate).map(letterToPriority).sum().value();
 };
 
 console.log(solve(readInput('example1.txt')), '\n\n\n');
 // console.log(solve(readInput('example2.txt')), '\n\n\n');
-// console.log(solve(readInput('puzzleInput.txt')), '\n\n\n');
+console.log(solve(readInput('puzzleInput.txt')), '\n\n\n');
