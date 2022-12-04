4e8b75040309cfd32ca1c02703f1981761d7a362
Richard Gill <richard@rgill.co.uk>
2022-12-04 07:58:20 +0000

    day 4

--- src/days/04.2/index.js
+++ src/days/04.2/index.js
@@ -3,11 +3,32 @@ import { readRelativeInput } from '@/common/file.js';
 
 const readInput = (fileName) => readRelativeInput(import.meta.url, fileName);
 
+const parseInput = (input) => {
+  const lines = input.trim().split('\n').map(
+    (line) =>
+      line.split(',').map(
+        (range) => {
+          const splitRange = range.split('-');
+          return {
+            min: parseInt(splitRange[0], 10),
+            max: parseInt(splitRange[1], 10),
+          };
+        },
+      ),
+  );
+  return lines;
+};
+
+const partialOverlap = ([range1, range2]) => {
+  return (range1.min >= range2.min && range1.min <= range2.max) || (range2.min >= range1.min && range2.min <= range1.max);
+};
+
 export const solve = (input) => {
-  console.log(input);
-  return 'answer';
+  const rangePairs = parseInput(input);
+  const fullyContainsPairs = rangePairs.filter(partialOverlap);
+  return fullyContainsPairs.length;
 };
 
 console.log(solve(readInput('example1.txt')), '\n\n\n');
 // console.log(solve(readInput('example2.txt')), '\n\n\n');
-// console.log(solve(readInput('puzzleInput.txt')), '\n\n\n');
+console.log(solve(readInput('puzzleInput.txt')), '\n\n\n');
